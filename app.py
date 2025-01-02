b388cf62cdb0129fe632c3417f92fff0ed19cbea
leekempson <leekempson@hotmail.com>
2025-01-02 13:04:19 +0100

    database read and write configured

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
 from flask import Flask
 from routes import pages
+import os
+from pymongo import MongoClient
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 def create_app():
     app = Flask(__name__)
-    app.register_blueprint(pages)
+    client = MongoClient(os.getenv("MONGODB_URI"))
+    app.db = client.tracker
 
+    app.register_blueprint(pages)
     return app
